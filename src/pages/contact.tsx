34edf33addac82b1cf378e01af14a8ede4d82d09
designpigofficial <89279319+designpigofficial@users.noreply.github.com>
2022-08-05 11:23:51 -0600

    contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,32 +2,43 @@ import * as React from 'react';
 
 import {
   Button,
+  Box,
   Heading,
   HStack,
-  Image,
-  Link,
   Text,
   VStack,
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
   Input,
+  StackDivider,
+  Textarea,
 } from '@chakra-ui/react';
 
 import Container from '@/components/Container';
 import Layout from '@/components/layout';
+import { title } from 'process';
+
+function Feature({ title, desc, ...rest }) {
+  return (
+    <Box p={5} shadow='md' borderWidth='1px' {...rest}>
+      <Heading fontSize='xl'>{title}</Heading>
+      <Text mt={4}>{desc}</Text>
+    </Box>
+  )
+}
 
 
-const ContactPage = () => {
+function ContactPage() {
   return (
     <Layout>
       <Container as='main'>
         <VStack
           as='section'
+          display='flex'
           justify='center'
+          alignItems='center'
           minHeight={{ base: 'calc(100vh - 7.5rem)', md: 'calc(100vh - 8rem)' }}
-          spacing={6}
+          spacing={10}
         >
           <Heading as='h1' color='primary' textAlign='center'>
             Contact me here
@@ -36,29 +47,40 @@ const ContactPage = () => {
             <FormControl>
               <FormLabel>Name:</FormLabel>
               <Input type='text' />
-              <FormHelperText>What should we call you?</FormHelperText>
               <FormLabel>Email:</FormLabel>
               <Input type='email' />
-              <FormHelperText>What's your email address?</FormHelperText>
               <FormLabel>Phone:</FormLabel>
               <Input type='phone' />
-              <FormHelperText>Care to share your number?</FormHelperText>
+              <FormLabel>Message:</FormLabel>
+              <Textarea></Textarea>
             </FormControl>
-            <Button
-              as='a'
-              href='/contact'
-              target='_blank'
-              rel='noopener noreferrer'
-              size='sm'
-              colorScheme='green'
-            >
-              Submit
-            </Button>
           </HStack>
+          <Button
+            as='a'
+            href='/contact'
+            target='_blank'
+            rel='noopener noreferrer'
+            size='sm'
+            colorScheme='green'
+          >
+            Submit
+          </Button>
         </VStack>
-      </Container>
+        <VStack
+          divider={<StackDivider borderColor='gray.200' />}
+          spacing={4}
+          align='stretch'
+        >
+          <Feature title={"Give us a call at..."} desc={"303.414.9536"}>
+
+          </Feature>
+          <Feature title={"Email us at..."} desc={"dedrickgrey2@gmail.com"}>
+
+          </Feature>
+          </VStack>
+        </Container>
     </Layout>
   );
-};
+}
 
 export default ContactPage;
