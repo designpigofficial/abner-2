df8da4094335fc32db92846e6709fb9acc1d401b
designpig@outlook.com <designpig@outlook.com>
2022-08-04 20:05:39 -0600

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ const HomePage = () => {
               target='_blank'
               rel='noopener noreferrer'
               size='sm'
-              colorScheme='orange'
+              colorScheme='grey'
             >
               Hire Me
             </Button>
